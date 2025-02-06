efff987e950c44fd956c9c56bc48b8d060e07b90
Tatsiana <96041733+bogdanovich231@users.noreply.github.com>
2025-02-06 18:19:19 +0100

    feat: created form with inputs. controlled state input and empty handler submit, style

--- src/Components/FormContainer/FormContainer.tsx
+++ src/Components/FormContainer/FormContainer.tsx
@@ -1,5 +1,45 @@
+import { useState } from 'react'
+import CustomTextInput from '../CustomTextInput/CustomTextInput'
+
 function FormContainer() {
-  return <div>Form</div>
+  const [formData, setFormData] = useState({ firstname: '', lastname: '', email: '' })
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target
+    setFormData((prev) => ({
+      ...prev,
+      [name]: value
+    }))
+  }
+
+  const handleSubmit = async (e: React.FormEvent) => {}
+
+  return (
+    <div className="flex flex-col items-center min-h-screen bg-[#F0EAF8]">
+      <h2 className="text-2xl font-medium text-[#000853]">Personal info</h2>
+      <form className="flex flex-col gap-[24px] w-full max-w-[426px]" onSubmit={handleSubmit}>
+        <CustomTextInput
+          labelName={'First Name'}
+          value={formData.firstname}
+          name="firstname"
+          onChange={handleChange}
+        />
+        <CustomTextInput
+          labelName={'Last Name'}
+          value={formData.lastname}
+          name="lastname"
+          onChange={handleChange}
+        />
+        <CustomTextInput
+          labelName={'Email Address'}
+          value={formData.email}
+          name="email"
+          onChange={handleChange}
+        />
+        <button type="submit">Send Application</button>
+      </form>
+    </div>
+  )
 }
 
 export default FormContainer
