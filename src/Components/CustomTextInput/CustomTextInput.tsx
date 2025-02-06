df591916fc6d43d079d4faeb9cf5f7c0e0d997e8
Tatsiana <96041733+bogdanovich231@users.noreply.github.com>
2025-02-06 18:47:19 +0100

    feat: added icon for error and change input color with error

--- src/Components/CustomTextInput/CustomTextInput.tsx
+++ src/Components/CustomTextInput/CustomTextInput.tsx
@@ -1,9 +1,11 @@
+import ErrorIcon from '../../assets/error-icon.svg';
+
 interface IFormFields {
-  labelName: string
-  value: string
-  name: string
-  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void
-  error?: string
+  labelName: string;
+  value: string;
+  name: string;
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  error?: string;
 }
 
 function CustomTextInput({ name, labelName, value, onChange, error }: IFormFields) {
@@ -12,16 +14,23 @@ function CustomTextInput({ name, labelName, value, onChange, error }: IFormField
       <label className="flex flex-col text-base text-[#000853]">
         {labelName}
         <input
-          className="w-full px-2 py-2 mt-[8px] rounded-[8px] border border-solid duration-300 easy-linear transition-colors border-[#CBB6E5] bg-white focus:border-2  border-[#761BE4] outline-none shadow-none"
+          className={`w-full px-2 py-2 mt-[8px] rounded-[8px] border border-solid duration-300 ease-linear transition-colors 
+          ${error ? 'border-red-500 bg-red-50 border-2' : 'border-[#CBB6E5] bg-white'} 
+          focus:border-2 focus:border-[#761BE4] outline-none shadow-none`}
           type="text"
           name={name}
           value={value}
           onChange={onChange}
         />
       </label>
-      {error && <span className="text-sm text-[#000853]">{error}</span>}
+      {error && (
+        <span className="flex align-center gap-[10px] mt-[8px] text-sm text-[#000853]">
+          <img src={ErrorIcon} alt="error icon" />
+          {error}
+        </span>
+      )}
     </div>
-  )
+  );
 }
 
-export default CustomTextInput
+export default CustomTextInput;
